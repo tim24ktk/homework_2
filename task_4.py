3dceb07428927df7fd834c20a02ab98d6671af91
tim24ktk <tim24ktk@mail.ru>
2022-11-27 20:59:24 +0300

    task_4 refactoring

--- task_4.py
+++ task_4.py
@@ -13,19 +13,25 @@ if value > 0:
     result = []
     product = 1
     flag = 0
+    values = []
 
     for i in range(-value, value + 1):
         result.append(i)
 
     for i, d in enumerate(indexes):
         if d.isnumeric() and int(d) < len(result):
+            values.append(result[int(d)])
+
             product *= result[int(d)]
         else:
             flag = 1
             break
 
     if flag == 0:
-        print(product)
+        print(f'Список из N элементов, заполненных числами из промежутка [-N, N]: {result}')
+        print(f'Индексы из файла: {indexes}')
+        print(f'Список элементов на указанных позициях: {values}')
+        print(f'Произведение элементов на позициях указанных в файле: {product}')
     else:
         print('Не удалось посчитать, т.к. один из индексов в файле больше чем длина списка или индекс указан не цифрами!')
 else:
